551c243b4cbc60d4d1d9fffadec4dfa537091dc8
krushnak39 <krushnak819@gmail.com>
2026-07-19 20:24:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let board = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let gameActive = true;
+
+const statusText = document.getElementById("status");
+
+const winningCombinations = [
+  [0,1,2], [3,4,5], [6,7,8],
+  [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8], [2,4,6]
+];
+
+function makeMove(index) {
+  if (board[index] !== "" || !gameActive) return;
+
+  board[index] = currentPlayer;
+  document.getElementsByClassName("cell")[index].innerText = currentPlayer;
+
+  if (checkWinner()) {
+    statusText.innerText = `Player ${currentPlayer} Wins!`;
+    gameActive = false;
+    return;
+  }
+
+  if (!board.includes("")) {
+    statusText.innerText = "It's a Draw!";
+    gameActive = false;
+    return;
+  }
+
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  statusText.innerText = `Player ${currentPlayer}'s Turn`;
+}
+
+function checkWinner() {
+  return winningCombinations.some(combo => {
+    return combo.every(index => board[index] === currentPlayer);
+  });
+}
+
+function resetGame() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  gameActive = true;
+  currentPlayer = "X";
+  statusText.innerText = "Player X's Turn";
+
+  const cells = document.getElementsByClassName("cell");
+  for (let cell of cells) {
+    cell.innerText = "";
+  }
+}
